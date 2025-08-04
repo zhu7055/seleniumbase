1a4197fbb1496142a71d00fb8d6a0c01bc8986bc
jiayi-zhu <118984711+zhu7055@users.noreply.github.com>
2025-08-04 16:18:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from seleniumbase import SB
 
 with SB(test=True, uc=True) as sb:
+    #Open your browser and visit the website
     sb.open("https://google.com/ncr")
+    #Search
     sb.type('[title="Search"]', "tech with tim\n")
+    #Click to go to the website
     sb.click('[href*="https://www.youtube.com/c/TechWithTim"]')
+    #save the screenshot
     sb.save_screenshot_to_logs()  # ./latest_logs/
     print(sb.get_page_title())
